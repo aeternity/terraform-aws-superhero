1750785ed5fe506d55700257ef526db20fc8a904
Petar Peshev <petar.peshev@leanplum.com>
2021-10-28 22:08:22 +0300

    deploy ipfs and add outputs

---
+++ ipfs.tf
@@ -0,0 +1,54 @@
+locals {
+  ipfs_name = "ipfs"
+  ipfs_tags = merge({ "Name" : local.ipfs_name }, local.standard_tags)
+}
+
+module "ipfs-sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.4.0"
+
+  name        = local.ipfs_name
+  description = "Security group for Ipfs"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 4001
+      to_port     = 4001
+      protocol    = "tcp"
+      description = "Ipfs ports"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+  egress_rules = ["all-all"]
+
+  tags = local.ipfs_tags
+}
+
+module "ipfs" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.2.0"
+
+  name                   = local.ipfs_name
+  key_name               = "temp"
+  instance_type          = "t2.medium"
+  ami                    = "ami-0eda1419e30a5a080"
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+  secondary_private_ips  = ["10.0.4.4"]
+  tags                   = local.ipfs_tags
+  vpc_security_group_ids = [module.ipfs-sg.security_group_id]
+}
+
+resource "aws_volume_attachment" "ipfs" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ipfs.id
+  instance_id = module.ipfs.id
+}
+
+resource "aws_ebs_volume" "ipfs" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  # size should be configured per requirements for each environment, we have to choose and disk type
+  size = 50
+  type = "gp3"
+  tags = local.ipfs_tags
+}
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,18 @@ output "postgres_endpoint" {
   value = module.superhero-backend-postgres.db_instance_endpoint
 }
 
-output "redis_private_ip" {
+output "superhero_backend_redis_private_ip" {
   value = module.superhero-backend-redis.private_ip
+}
+
+output "graffiti_redis_redis_private_ip" {
+  value = module.graffiti-redis.private_ip
+}
+
+output "ipfs_private_ip" {
+  value = module.ipfs.private_ip
+}
+
+output "ipfs_public_ip" {
+  value = module.ipfs.public_ip
 }
