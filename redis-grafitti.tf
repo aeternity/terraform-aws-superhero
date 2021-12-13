8c1667ab3dd2b0eb49ab950bcbe22c346b8b9293
Petar Peshev <petar.peshev@leanplum.com>
2021-12-13 23:01:59 +0200

    add redis grafitti monitoring sg rules

--- redis-grafitti.tf
+++ redis-grafitti.tf
@@ -12,6 +12,22 @@ module "graffiti-redis-sg" {
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
   ingress_cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 9121
+      to_port     = 9121
+      protocol    = "tcp"
+      description = "Redis exporter ports"
+      cidr_blocks = data.terraform_remote_state.vpc.outputs.vpc_cidr_block
+    },
+    {
+      from_port   = 9100
+      to_port     = 9100
+      protocol    = "tcp"
+      description = "Node exporter ports"
+      cidr_blocks = data.terraform_remote_state.vpc.outputs.vpc_cidr_block
+    },
+  ]
   ingress_rules       = ["redis-tcp"]
   egress_rules        = ["all-all"]
 
