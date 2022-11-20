7a61255a125c4c15b8e0aa9b0dd44120d5254a99
petarpeshev <pe6ev@yahoo.com>
2022-11-21 01:05:16 +0200

    add iam user and policy for graffiti bucket

--- s3-bucket-graffiti.tf
+++ s3-bucket-graffiti.tf
@@ -8,3 +8,14 @@ module "s3_bucket_graffiti" {
     enabled = false
   }
 }
+
+module "graffiti_s3_user" {
+  source = "cloudposse/iam-s3-user/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "1.0.0"
+  namespace    = "aeternity"
+  stage        = "${local.env_human}"
+  name         = "graffiti-backend-s3"
+  s3_actions   = ["s3:GetObject", "s3:PutObject"]
+  s3_resources = ["${module.s3_bucket_graffiti.s3_bucket_arn}/*"]
+}
